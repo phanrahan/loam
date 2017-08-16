5e23f8508ffd043fe656ef5c1c18f0c1d4604556
Pat Hanrahan <hanrahan@cs.stanford.edu>
2017-08-16 13:42:03 -0700

    New version of ice40 test harness.

--- tests/icestick/mantle40/harness/testharness.py
+++ tests/icestick/mantle40/harness/testharness.py
@@ -3,8 +3,9 @@ if sys.version_info < (3, 3):
     from funcsigs import signature
 else:
     from inspect import signature
-from magma import wire, compile
-from mantle import Or, XOr, DFF, Counter
+from magma import wire, compile, EndCircuit
+from loam.boards.icestick import IceStick
+from mantle import Not, Or, XOr, DFF, Counter
 from mantle.lattice.ice40 import ROMB
 
 # check that number of function arguments equals number of circuit inputs
@@ -17,7 +18,7 @@ def check(circuit, func):
     for name, port in circuit.interface.ports.items():
         if port.isinput(): 
             ncircargs += 1
-    print(nfuncargs, ncircargs)
+    #print(nfuncargs, ncircargs)
     assert nfuncargs == ncircargs
     return nfuncargs
 
@@ -32,12 +33,14 @@ def testvectors(func):
     for x in range(M):
         args = [(x >> i) & 1 for i in range(nfuncargs)]
         y = 1 if func(*args) else 0
-        print(args, y)
+        #print(args, y)
         rom[x] = (y << nfuncargs) | (x & ((1 << nfuncargs)-1))
     return rom
 
-def test(circuit, func):
-    nfuncargs = check(circuit, func)
+def generate_test(Circuit, func):
+    #nfuncargs = check(circuit, func)
+    sig = signature(func)
+    nfuncargs = len(sig.parameters)
     assert nfuncargs+1 < 8
 
     rom = testvectors( func )
@@ -48,18 +51,49 @@ def test(circuit, func):
     wire( 1, romb.RCLKE )
     wire( counter.O, romb.RADDR )
 
+    circuit = Circuit(nfuncargs)
+
     circuit(*[romb.RDATA[i] for i in range(nfuncargs)])
-
-    xor = XOr(2)
-    xor(circuit.O, romb.RDATA[nfuncargs])
-
-    orr = Or(2)
-    error = DFF()
-    e =  error(orr(xor.O, error.O))
 
     orr = Or(2)
     finished = DFF()
     f = finished(orr(counter.COUT, finished.O))
 
+    xor = XOr(2)
+    xor(circuit.O, romb.RDATA[nfuncargs])
+    orr = Or(2)
+    error = DFF()
+    e =  error(orr(xor.O, error.O))
+
     return f, e
+
+
+def test(circuit, func):
+
+    icestick = IceStick()
+    icestick.Clock.on()
+    icestick.DSR.on()
+    icestick.CTS.on()
+    icestick.D1.on()
+    icestick.D2.on()
+    icestick.D3.on()
+    icestick.D4.on()
+    icestick.D5.on()
+    
+    main = icestick.main()
+    
+    finished, error = generate_test(circuit, func)
+
+    wire(finished, main.D3)
+    wire(finished, main.D4)
+    wire(finished, main.DSR)
+    
+    wire(error, main.D1)
+    wire(error, main.D2)
+    wire(Not()(error), main.D5)
+    wire(error, main.CTS)
+
+    EndCircuit()
+
+    return main
 
