6d408302e50111fd484efd46ca95e03758aaac93
Pat Hanrahan <hanrahan@cs.stanford.edu>
2017-08-30 07:56:22 -0700

    Added gated clock example/test.

---
+++ tests/icestick/mantle40/register/gatedreg4.py
@@ -0,0 +1,17 @@
+from magma import wire, clock, bit, compile, EndCircuit
+from loam.boards.icestick import IceStick, Register
+
+icestick = IceStick()
+icestick.Clock.on()
+for i in range(4):
+    icestick.J1[i].input().on()
+    icestick.J3[i].output().on()
+icestick.J1[4].input().on()
+
+main = icestick.main()
+
+reg = Register(4)
+wire( clock( bit(main.CLK) & main.J1[4] ), reg.CLK ) 
+wire( reg(main.J1), main.J3 )
+
+EndCircuit()
