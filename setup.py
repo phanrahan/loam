ec83d12d44c5926b43c5344451fb453ccaf0cc50
Lenny Truong <lenny@stanford.edu>
2018-05-18 11:27:37 -0700

    Remove requirements.txt

--- setup.py
+++ setup.py
@@ -1,10 +1,5 @@
 from setuptools import setup
 
-from pip.req import parse_requirements
-
-# parse_requirements() returns generator of pip.req.InstallRequirement objects
-install_reqs = parse_requirements("requirements.txt", session=False)
-
 reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
@@ -12,6 +7,4 @@ setup(
     version='0.1-alpha',
     description='Model boards and parts using Magma',
     packages=["loam"],
-    
-    install_requires=reqs
     )
