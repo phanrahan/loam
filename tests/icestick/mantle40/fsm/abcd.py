4bdf89674540ab281cc83b9a4fd09a6d21635af2
Pat Hanrahan <hanrahan@cs.stanford.edu>
2018-06-20 10:10:20 -0700

    Updated abcd.py to the new DefineFSM interface.

--- tests/icestick/mantle40/fsm/abcd.py
+++ tests/icestick/mantle40/fsm/abcd.py
@@ -5,7 +5,7 @@ import magma as m
 from mantle import Counter
 from mantle.util.debounce import debounce
 from mantle.util.edge import falling
-from mantle.util.fsm import FSM
+from mantle.util.fsm import DefineFSM
 from loam.boards.icestick import IceStick
 
 icestick = IceStick()
@@ -17,22 +17,23 @@ icestick.J1[4].input().on()
 
 main = icestick.main()
 
-myfsm = fsm( alphabet = ['0', '1', '2', '3'], 
-                      states = ['A', 'B', 'C', 'D'], 
-                      initial = 'A', 
-                      finals = {}, 
-                      map = { 
-                         'A': {'1': 'B'},
-                         'B': {'2': 'C', '3': 'D'},
-                         'C': {'3': 'D', '0': 'A'},
-                         'D': {'0': 'A'},
-                        } )
+myfsm = fsm( alphabet = {'0', '1', '2', '3'}, 
+             states = {'A', 'B', 'C', 'D'}, 
+             initial = 'A', 
+             finals = set(), 
+             map = { 
+                 'A': {'1': 'B'},
+                 'B': {'2': 'C', '3': 'D'},
+                 'C': {'3': 'D', '0': 'A'},
+                 'D': {'0': 'A'},
+             } )
 
 slow = Counter(16)
 select = debounce( main.J1[4], slow.COUT )
 pulse = falling( select )
 
-f = FSM( myfsm )
+FSM = DefineFSM( 'ABCD', myfsm )
+f = FSM()
 
 m.wire( main.J1[0:4], f.events )
 m.wire( pulse, f.CE )
