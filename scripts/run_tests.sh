d8fb7895d5ce882d466473afa003b5704eed572e
Pat Hanrahan <hanrahan@cs.stanford.edu>
2018-07-09 18:37:56 -0700

    Add test script.

---
+++ scripts/run_tests.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -x
+
+err=0
+# Use a trap to check for errors, we do this over set -e so that all the tests
+# run (so that we may catch multiple errors)
+trap 'err=1' ERR
+
+py.test --target ice40    tests/test_mantle tests/icestick
+py.test --target spartan3 tests/test_mantle
+py.test --target spartan6 tests/test_mantle
+cd tests
+./bake
+cd ..
+
+# Return code is nonzero if an error occured
+test $err = 0
